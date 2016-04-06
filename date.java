3a4b7420a40541e70fa5308e9d724acaff952e8c
e5jgutiv01 <e5jgutiv01@pc32.(none)>
2016-04-06 11:50:04 +0200

    HEY

---
+++ date.java
@@ -0,0 +1,40 @@
+class Date{
+
+private int year;
+private int month;
+private int day;
+
+	public Date (int year, int month, int day){
+	
+	switch (month){
+	case 1: //next
+	case 3: //next
+	case 5: //next
+	case 7: //next
+	case 8: //next
+	case 10: //next
+	case 12:
+		if (month>=1 && month <=31){
+		
+		}
+	break;
+
+	case 2:
+		if (month>=1 && month <=28){
+		
+		}
+	break;
+	case 4: //next
+	case 6: //next
+	case 9: //next
+	case 11:
+		if (month>=1 && month <=30){
+		
+		}
+
+	}
+
+
+	}
+
+}
